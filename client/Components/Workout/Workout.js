ff1a66be4dffb192346d4a77456b4df5d79d5bca
Inderprit Singh <singhinderprit1992@gmail.com>
2024-09-04 16:51:07 -0500

    set up links in header and some barebones routing

---
+++ client/Components/Workout/Workout.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Workout = () => {
+  return <div>Single Workout Component</div>;
+};
---
+++ client/Components/WorkoutList/WorkoutList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const WorkoutList = () => {
+  return <div>Workout List Component</div>;
+};
